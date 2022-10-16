039777737de5cfb9077d8cb8d851c8bf89c298a6
Michael Juckes <michaelpauljuckes@gmail.com>
2022-10-16 11:41:45 +0100

    making minor adjustments and setting up on linux home machine

--- src/App.svelte
+++ src/App.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <main>
-	<h1>Hello {name} today!</h1>
+	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
@@ -16,10 +16,11 @@
 	}
 
 	h1 {
-		color: #ff3e00;
+		color: #975305;
 		text-transform: uppercase;
-		font-size: 4em;
+		font-size: 5em;
 		font-weight: 100;
+		font-family:  sans-serif, Arial, Helvetica;
 	}
 
 	@media (min-width: 640px) {
